7f0d4b734c9b92e0e1f6c182be9fa54372214b7c
Jan <jeligio808@gmail.com>
2020-09-12 23:52:30 -1000

    Created Navigation Bar

---
+++ client/pages/NavigationBar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+    View,
+    Text
+  } from 'react-native';
+
+const NavigationBar = () => {
+    return(
+        <View style={{height: 75, backgroundColor:'purple'}}>
+          <View>
+              <Text>Fill</Text>
+          </View>
+          <View>
+              <Text>Fill</Text>
+          </View>
+          <View>
+              <Text>Fill</Text>
+          </View>
+          <View>
+              <Text>Fill</Text>
+          </View>
+          <View>
+              <Text>Fill</Text>
+          </View>
+        </View>
+    );
+
+};
+
+  export default NavigationBar;
