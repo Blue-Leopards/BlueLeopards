ac54a5bfc46f3c6d3a2a0a852066d087328d0859
Jan <jeligio808@gmail.com>
2020-10-07 16:43:56 -1000

    Update Navigation

--- client/pages/NavigationBar.js
+++ client/pages/NavigationBar.js
@@ -1,43 +1,35 @@
 import React from 'react';
-import {
-    View,
-    Text
-  } from 'react-native';
 
-const NavigationBar = () => {
+import { BottomNavigation, Text } from 'react-native-paper';
+
+const ProfilesRoute = () => <Text>Profiles</Text>;
+const ProjectsRoute = () => <Text>Projects</Text>;
+const InterestsRoute = () => <Text>Interests</Text>;
+const SettingsRoute = () => <Text>Settings</Text>;
+
+const NavigationBar = ({ navigation }) => {
+    const [index, setIndex] = React.useState(0);
+    const [routes] = React.useState([
+      { key: 'profiles', title: 'Profiles', icon: '' },
+      { key: 'projects', title: 'Projects', icon: '' },
+      { key: 'interests', title: 'Interests', icon: '' },
+      { key: 'settings', title: 'Settings', icon: '' },
+    ]);
+
+    const renderScene = BottomNavigation.SceneMap({
+      profiles: ProfilesRoute,
+      projects: ProjectsRoute,
+      interests: InterestsRoute,
+      settings: SettingsRoute
+    });
+
     return(
-        <View style={{
-            height: 75, 
-            backgroundColor:'grey',
-            flexDirection: 'row'}}>
-          <View style={{flex: 1,                 
-                justifyContent: 'center',
-                alignItems: 'center'}}>
-              <Text>Profiles</Text>
-          </View>
-          <View style={{flex: 1,                 
-                justifyContent: 'center',
-                alignItems: 'center'}}>
-              <Text>Projects</Text>
-          </View>
-          <View style={{flex: 1,                 
-                justifyContent: 'center',
-                alignItems: 'center'}}>
-              <Text>Interests</Text>
-          </View>
-          <View style={{flex: 1,                 
-                justifyContent: 'center',
-                alignItems: 'center'}}>
-              <Text>Filter</Text>
-          </View>
-          <View style={{flex: 1,                 
-                justifyContent: 'center',
-                alignItems: 'center'}}>
-              <Text>Account</Text>
-          </View>
-        </View>
+        <BottomNavigation
+            navigationState={{ index, routes}}
+            onIndexChange={setIndex}
+            renderScene={renderScene}
+        />
     );
-
 };
 
   export default NavigationBar;
