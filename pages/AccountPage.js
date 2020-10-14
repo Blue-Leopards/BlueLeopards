b40e11f9c57afc81c3bb8ed0950d435d02514da1
Jan <jeligio808@gmail.com>
2020-10-14 00:00:53 -1000

    Update Account Page

--- pages/AccountPage.js
+++ pages/AccountPage.js
@@ -1,22 +1,15 @@
-import React from 'react';
-import {
-    View,
-} from 'react-native';
-
-import {
-    TextInput,
-    Button,
-    Title
-} from 'react-native-paper';
+import React, { useState } from 'react';
+import { View } from 'react-native';
+import { TextInput, Button, Title } from 'react-native-paper';
+import { useAuth } from "../providers/AuthProvider";
 
 
-const MyInput = (props) => {
-    const [text, setText] = React.useState('');
+const MyInput = ({category, value, onChangeHandler}) => {
+    const [text, setText ] = useState("");
 
     return (
         <TextInput
-            label={props.category}
-            placeholder={props.placeholder}
+            label={category}
             value={text}
             onChangeText={text => setText(text)}
         />
@@ -24,17 +17,49 @@ const MyInput = (props) => {
 };
 
 const AccountPage = () => {
-
+    const { user } = useAuth();
+    // console.log(Object.getOwnPropertyNames(user.));
+    // console.log(user.customData);
+    const {_id, email, firstName, lastName, bio, title, picture} = user.customData;
+    
+    const [ userData, setUserData ] = useState({
+        _id, email, firstName, lastName, bio, title, picture
+    });
+    console.log(userData);
     return (
         <View style={{padding:40}}>
             <Title>Account Settings</Title>
-            <MyInput category="First Name"/>
-            <MyInput category="Last Name"/>
-            <MyInput category="Email"/>
-            <MyInput category="Biographical statement"/>
+            <TextInput
+                label="Email"
+                value={userData.email}
+                onChangeText={text => setUserData({...userData, email: text})}
+                />
+            <TextInput
+                label="FirstName"
+                value={userData.firstName}
+                onChangeText={text => setUserData({...userData, firstName: text})}
+                />
+            <TextInput
+                label="Last Name"
+                value={userData.lastName}
+                onChangeText={text => setUserData({...userData, lastName: text})}
+                />
+            <TextInput
+                label="Title"
+                value={userData.title}
+                onChangeText={text => setUserData({...userData, title: text})}
+                />
+            <TextInput
+                label="Bio"
+                value={userData.bio}
+                onChangeText={text => setUserData({...userData, bio: text})}
+                />
+            <TextInput
+                label="Picture URL"
+                value={userData.picture}
+                onChangeText={text => setUserData({...userData, picture: text})}
+                />
 
-            <MyInput category="Title"/>
-            <MyInput category="Picture URL"/>
             <MyInput category="Interests"/>
             <MyInput category="Projects"/>
             <Button
