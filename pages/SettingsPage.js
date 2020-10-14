eae4b1e02472ee6e50a6ce3b3749f2159673ff6d
Jan <jeligio808@gmail.com>
2020-10-13 23:15:31 -1000

    Add Manage Account Button to Settings Page

--- pages/SettingsPage.js
+++ pages/SettingsPage.js
@@ -2,23 +2,17 @@ import React from 'react';
 import { View, Text, } from 'react-native';
 import { Logout } from '../components/Logout';
 import { Button } from 'react-native-paper';
-
 import { useNavigation } from "@react-navigation/native";
-
-const styles = { 
-    flex:1,
-    alignItems: 'center',
-    justifyContent: 'center',
-};
 
 const SettingsPage = () => {
     const navigation = useNavigation();
 
     return (
             <View style={{
-                padding: 20
+                padding: 30,
             }}>
-                <Button mode="contained" onPress={() => navigation.navigate("AddProject")}>Create Project</Button>
+                <Button style={{marginBottom: 5}} mode="contained" onPress={() => navigation.navigate("Account")}>Manage Account</Button>
+                <Button style={{marginBottom: 5}} mode="contained" onPress={() => navigation.navigate("AddProject")}>Create Project</Button>
                 <Logout/>
             </View>
     );
